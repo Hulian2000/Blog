f5ee67c60a439f58cbbecabf75890dea7bbdc528
Hulian2000 <prepmoringa11@gmail.com>
2019-09-19 16:22:21 +0300

    add files

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,10 @@ from flask import render_template, redirect,request,url_for,abort
 from . import main
 from flask_login import login_required,current_user
 from ..models import User,Role
-from .forms import UpdateProfile,PitchNow,MyComment,UpVote,DownVote
 from .. import db
-
+@main.route('/')
+def index():
+    return render_template('index.html')
 
 @main.route('/user/<uname>')
 @login_required
--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
-app = create_app('development')
-app = create_app('production')
+app = create_app()
 
 
 manager = Manager(app)
